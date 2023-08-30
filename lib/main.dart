ded87cbdd78f7da1bd2e4eebb09f0b69f469f2a2
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2023-08-30 22:57:04 +0700

    call publish API

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +23,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyAppState extends ChangeNotifier {}
+class MyAppState extends ChangeNotifier {
+  var callPublishMsgsDone = false;
+}
 
 class TabBarExample extends StatelessWidget {
   const TabBarExample({super.key});
@@ -64,24 +69,123 @@ class BuildPublishWidget extends StatefulWidget {
 }
 
 class _BuildPublishWidgetState extends State<BuildPublishWidget> {
-  final myController = TextEditingController();
+  final messageBodyController = TextEditingController();
+  final numMessageController = TextEditingController();
+  final topicController = TextEditingController();
+  Future<PublishResponse>? _publishResponse;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TextFormField(
-          maxLines: 20,
-          controller: myController,
+          maxLines: 5,
+          controller: messageBodyController,
+          decoration: const InputDecoration(
+            labelText: 'Message',
+          ),
         ),
-        const SizedBox(height: 10),
+        TextFormField(
+          maxLines: 1,
+          controller: numMessageController,
+          decoration: const InputDecoration(
+            labelText: 'Number Of message',
+          ),
+        ),
+        TextFormField(
+          maxLines: 1,
+          controller: topicController,
+          decoration: const InputDecoration(
+            labelText: 'Topic',
+          ),
+        ),
+        // Reponse
+        (_publishResponse == null)
+            ? TextFormField(
+                maxLines: 5,
+                decoration: const InputDecoration(
+                  labelText: 'Response',
+                ),
+              )
+            : buildFutureBuilder(),
+        const SizedBox(height: 5),
         ElevatedButton(
           onPressed: () {
-            print("Text: ${myController.text}");
+            setState(() {
+              var numOfMsg = int.tryParse(numMessageController.text);
+              _publishResponse = publish(
+                messageBodyController.text,
+                numOfMsg ?? 1, // if numOfMsg is null default value is 1
+                topicController.text,
+              );
+            });
           },
           child: const Text("Publish"),
         )
       ],
+    );
+  }
+
+  FutureBuilder<PublishResponse> buildFutureBuilder() {
+    return FutureBuilder(
+      future: _publishResponse,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return TextFormField(
+            maxLines: 5,
+            initialValue:
+                "Total: ${snapshot.data!.totalMessage} Success: ${snapshot.data!.success} Failed: ${snapshot.data!.failed}",
+          );
+        } else if (snapshot.hasError) {
+          return TextFormField(
+            maxLines: 5,
+            initialValue: "${snapshot.error}",
+          );
+        }
+        return const CircularProgressIndicator();
+      },
+    );
+  }
+}
+
+Future<PublishResponse> publish(
+    String message, int numOfMsg, String topic) async {
+  final response = await http
+      .post(
+        Uri.parse("http://127.0.0.1:9000"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, dynamic>{
+          'message': message,
+          'topic': topic,
+          'quantity': numOfMsg,
+        }),
+      )
+      .timeout(const Duration(seconds: 60));
+
+  if (response.statusCode == 201) {
+    return PublishResponse.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to publish message');
+  }
+}
+
+class PublishResponse {
+  final int totalMessage;
+  final int success;
+  final int failed;
+
+  const PublishResponse(
+      {required this.totalMessage,
+      required this.success,
+      required this.failed});
+
+  factory PublishResponse.fromJson(Map<String, dynamic> json) {
+    return PublishResponse(
+      totalMessage: json['totalMessage'],
+      success: json['success'],
+      failed: json['failed'],
     );
   }
 }
