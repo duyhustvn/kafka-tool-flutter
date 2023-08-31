2a2c1383e0c35464a0913458962313af5f282bfd
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2023-08-31 16:29:45 +0700

    update UI

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       create: (context) => MyAppState(),
       child: MaterialApp(
         theme: ThemeData(useMaterial3: true),
-        home: const TabBarExample(),
+        home: const KafkaTabBar(),
       ),
     );
   }
@@ -27,8 +27,8 @@ class MyAppState extends ChangeNotifier {
   var callPublishMsgsDone = false;
 }
 
-class TabBarExample extends StatelessWidget {
-  const TabBarExample({super.key});
+class KafkaTabBar extends StatelessWidget {
+  const KafkaTabBar({super.key});
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -76,50 +76,104 @@ class _BuildPublishWidgetState extends State<BuildPublishWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          maxLines: 5,
-          controller: messageBodyController,
-          decoration: const InputDecoration(
-            labelText: 'Message',
-          ),
-        ),
-        TextFormField(
-          maxLines: 1,
-          controller: numMessageController,
-          decoration: const InputDecoration(
-            labelText: 'Number Of message',
-          ),
-        ),
-        TextFormField(
-          maxLines: 1,
-          controller: topicController,
-          decoration: const InputDecoration(
-            labelText: 'Topic',
-          ),
-        ),
-        // Reponse
-        TextFormField(
-          maxLines: 3,
-          controller: responseController,
-        ),
-        const SizedBox(height: 5),
-        ElevatedButton(
-          onPressed: () async {
-            var numOfMsg = int.tryParse(numMessageController.text);
-            var res = await publish(
-              messageBodyController.text,
-              numOfMsg ?? 1, // if numOfMsg is null default value is 1
-              topicController.text,
-            );
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(height: 7),
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  maxLines: 1,
+                  controller: topicController,
+                  decoration: InputDecoration(
+                    labelText: 'Topic',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 7,
+              ),
+              Expanded(
+                child: TextFormField(
+                  maxLines: 1,
+                  controller: numMessageController,
+                  decoration: InputDecoration(
+                    labelText: 'Number Of message',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 7,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  var numOfMsg = int.tryParse(numMessageController.text);
+                  var res = await publish(
+                    messageBodyController.text,
+                    numOfMsg ?? 1, // if numOfMsg is null default value is 1
+                    topicController.text,
+                  );
 
-            responseController.text =
-                "total message: ${res.totalMessage} \nsuccess: ${res.success} \nfailed: ${res.failed}";
-          },
-          child: const Text("Publish"),
-        )
-      ],
+                  responseController.text =
+                      "total message: ${res.totalMessage} \nsuccess: ${res.success} \nfailed: ${res.failed}";
+                },
+                child: const Text("Publish"),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 7,
+          ),
+
+          TextFormField(
+            maxLines: 5,
+            controller: messageBodyController,
+            decoration: InputDecoration(
+              labelText: 'Message',
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 7,
+          ),
+
+          // Reponse
+          TextFormField(
+            maxLines: 3,
+            controller: responseController,
+            decoration: InputDecoration(
+              labelText: 'Response',
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+          ),
+          const SizedBox(height: 7),
+        ],
+      ),
     );
   }
 }
